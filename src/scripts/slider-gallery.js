09830528cfe10d9521793dd8397072ffea36a1d3
smerch88 <maxarmyk@gmail.com>
2022-10-10 21:27:43 +0300

    added some sorrect sizes of pictures in gallery section

--- src/scripts/slider-gallery.js
+++ src/scripts/slider-gallery.js
@@ -1,34 +1,33 @@
 const sliderGallery = () => {
-    let slideIndex = 1,
+  let slideIndex = 1,
     slides = document.querySelectorAll('.gallery__img');
 
-    showSlides(slideIndex);
+  showSlides(slideIndex);
 
-    function showSlides(n) {
-        
-        if(n > slides.length) {
-            slideIndex = 1;
-        }
-
-        if(n < 1) {
-            slideIndex = slides.length;
-        }
-
-        slides.forEach((item) => {
-            item.classList.add('swing-in-top-fwd');
-            item.style.display = 'none';
-        });
-        
-        slides[slideIndex - 1].style.display = 'block';
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
     }
 
-    function plusSlides (n) {
-        showSlides(slideIndex += n);
+    if (n < 1) {
+      slideIndex = slides.length;
     }
 
-    setInterval( () => {
-        plusSlides(1);
-    }, 7000);
-}
+    slides.forEach(item => {
+      item.classList.add('swing-in-top-fwd');
+      item.style.display = 'none';
+    });
+
+    slides[slideIndex - 1].style.display = 'block';
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  setInterval(() => {
+    plusSlides(1);
+  }, 7000);
+};
 
 sliderGallery();
