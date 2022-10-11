02a786274171a794d2c046bfd1d83793963dc1cd
smerch88 <maxarmyk@gmail.com>
2022-10-11 15:01:17 +0300

    loaction modal

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -27,3 +27,18 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-local-franchise]'),
+    closeModalBtn: document.querySelectorAll('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
