c69b0748d78ded716c79fd6902a4b8d31a68b159
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-10-11 20:26:31 +0000

    Deploying to gh-pages from @ InnaForkert/team-project-1@52f62364b327d4d47c045aa39f5822cdf0d53f56 🚀

---
+++ index.a86e7d50.js
@@ -0,0 +1,2 @@
+var mybutton=document.getElementById("myBtn");function scrollFunction(){document.body.scrollTop>20||document.documentElement.scrollTop>20?mybutton.style.display="block":mybutton.style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=function(){scrollFunction()};
+//# sourceMappingURL=index.a86e7d50.js.map
