8bb8c543bbc4145e735362dc8da34d96277db38b
smerch88 <maxarmyk@gmail.com>
2022-10-11 16:26:58 +0300

    franchise modal without js

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -16,7 +16,7 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open-local]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
+    closeModalBtn: document.querySelectorAll('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
