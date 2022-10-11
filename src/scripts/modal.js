ba0c1a8287a7071013bfd834b6dae694b0c42876
smerch88 <maxarmyk@gmail.com>
2022-10-11 15:32:58 +0300

    modal fix

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -16,22 +16,7 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open-local]'),
-    closeModalBtn: document.querySelectorAll('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
-
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-local-franchise]'),
-    closeModalBtn: document.querySelectorAll('[data-modal-close]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
