dde8fbb1a00f71a113a48490c0d100fb791d046d
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-10-11 17:57:01 +0300

    modals fix

---
+++ src/scripts/buy-now-modal.js
@@ -0,0 +1,52 @@
+const modalBM = document.querySelector('[data-buy-mobile-modal]');
+const overlayBM = document.querySelector('[data-buy-mobile-overlay]');
+const openModalBtnBM = document.querySelector('[data-buy-mobile-open]');
+const closeModalBtnBM = document.querySelector('[data-buy-mobile-close]');
+
+const openModalBM = function () {
+  modalBM.classList.remove('is-hidden');
+  overlayBM.classList.remove('is-hidden');
+};
+
+openModalBtnBM.addEventListener('click', openModalBM);
+
+const closeModalBM = function () {
+  modalBM.classList.add('is-hidden');
+  overlayBM.classList.add('is-hidden');
+};
+
+closeModalBtnBM.addEventListener('click', closeModalBM);
+overlayBM.addEventListener('click', closeModalBM);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modalBM.classList.contains('is-hidden')) {
+    closeModalBM();
+  }
+});
+
+///desktop
+const modalBD = document.querySelector('[data-buy-desktop-modal]');
+const overlayBD = document.querySelector('[data-buy-desktop-overlay]');
+const openModalBtnBD = document.querySelector('[data-buy-desktop-open]');
+const closeModalBtnBD = document.querySelector('[data-buy-desktop-close]');
+
+const openModalBD = function () {
+  modalBD.classList.remove('is-hidden');
+  overlayBD.classList.remove('is-hidden');
+};
+
+openModalBtnBD.addEventListener('click', openModalBD);
+
+const closeModalBD = function () {
+  modalBD.classList.add('is-hidden');
+  overlayBD.classList.add('is-hidden');
+};
+
+closeModalBtnBD.addEventListener('click', closeModalBD);
+overlayBD.addEventListener('click', closeModalBD);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modalBD.classList.contains('is-hidden')) {
+    closeModalBD();
+  }
+});
