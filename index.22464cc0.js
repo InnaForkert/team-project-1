ca503fe98ccbda5472d74a36bb6c21db383dee42
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-10-12 12:31:23 +0000

    Deploying to gh-pages from @ InnaForkert/team-project-1@7db7f4dddcd2491daa86cc3bf02d6af5947de931 🚀

---
+++ index.22464cc0.js
@@ -0,0 +1,2 @@
+var sliderGallery=function(){var l=1,e=document.querySelectorAll(".gallery__img");function n(n){n>e.length&&(l=1),n<1&&(l=e.length),e.forEach((function(l){l.classList.add("swing-in-top-fwd"),l.style.display="none"})),e[l-1].style.display="block"}n(l),setInterval((function(){n(l+=1)}),5e3)};sliderGallery();
+//# sourceMappingURL=index.22464cc0.js.map
